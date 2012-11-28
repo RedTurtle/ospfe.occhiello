47792c379d848fa6b1f665d3e081f32beea1e07e
Luca Fabbri <luca@keul.it>
2012-11-28 15:49:10 +0100

    added proper pypi classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='ospfe.occhiello',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta",
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
